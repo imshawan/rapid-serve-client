99edc85006237bbe6a2be9da1b5762fd2efdb156
Shawan Mandal <shawan.sm049@gmail.com>
2025-04-09 23:06:11 +0530

    fix: react dom rendering specific warnings and errors

--- components/dashboard/preview/image.tsx
+++ components/dashboard/preview/image.tsx
@@ -11,11 +11,11 @@ interface ImageProps {
 export default function Image({ file, data, outerClassname, className }: ImageProps) {
   return (
     <div className={cn("w-full h-full flex items-center justify-center p-4", outerClassname)}>
-      <img
+      {data ? <img
         src={data}
         alt={file.fileName}
         className={cn("h-full object-contain p-0 h-screen sm:h-[calc(100vh-100px)]", className)}
-      />
+      /> : ""}
     </div>
   )
 }
